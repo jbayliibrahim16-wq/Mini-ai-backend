cdc10dd3e8221946c6c02e880019b0892f4aba96
jbayliibrahim16-wq <jbayliibrahim16@gmail.com>
2026-06-12 02:40:16 +0300

    Add home page route

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // Middleware
+app.get('/', (req, res) => {
+  res.json({ 
+    status: 'Live', 
+    message: 'Mini AI Backend شغال 🔥',
+    time: new Date()
+  });
+});
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
